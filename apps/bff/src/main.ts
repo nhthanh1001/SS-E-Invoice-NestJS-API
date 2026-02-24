7750fba782d90d0e3c97b928313a856230fd41b3
Hai Thanh <haithanh.nguyen@estuary.solutions>
2026-02-24 22:33:48 +0700

    chore/#prettier: Setup prettier

--- apps/bff/src/main.ts
+++ apps/bff/src/main.ts
@@ -9,39 +9,42 @@ import { AppModule } from './app/app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(
-    new ValidationPipe({
-      transform: true,
-    }),
-  );
-  const globalPrefix = AppModule.CONFIGURATION.GLOBAL_PREFIX;
-  app.setGlobalPrefix(globalPrefix);
-  // Swagger Documentation
-  const config = new DocumentBuilder()
-    .setTitle('E-Invoice-bff API')
-    .setDescription('The E-Invoice-bff API description')
-    .setVersion('1.0.0')
-    .addBearerAuth({
-      description: 'Default JWT Authorization',
-      type: 'http',
-      in: 'header',
-      scheme: 'bearer',
-      bearerFormat: 'JWT',
-      name: 'Authorization',
-    })
-    .build();
-  const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup(`${globalPrefix}/docs`, app, documentFactory);
+  try {
+    const app = await NestFactory.create(AppModule);
+    app.useGlobalPipes(
+      new ValidationPipe({
+        transform: true,
+      }),
+    );
+    const globalPrefix = AppModule.CONFIGURATION.GLOBAL_PREFIX;
+    app.setGlobalPrefix(globalPrefix);
+    app.enableCors({
+      origin: '*',
+    });
+    // Swagger Documentation
+    const config = new DocumentBuilder()
+      .setTitle('E-Invoice-bff API')
+      .setDescription('The E-Invoice-bff API description')
+      .setVersion('1.0.0')
+      .addBearerAuth({
+        description: 'Default JWT Authorization',
+        type: 'http',
+        in: 'header',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'Authorization',
+      })
+      .build();
+    const documentFactory = () => SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup(`${globalPrefix}/docs`, app, documentFactory);
 
-  const port = AppModule.CONFIGURATION.APP_CONFIG.PORT;
-  await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
-  );
-  Logger.log(
-    `📖 Swagger documentation is available on: http://localhost:${port}/${globalPrefix}/docs`,
-  );
+    const port = AppModule.CONFIGURATION.APP_CONFIG.PORT;
+    await app.listen(port);
+    Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
+    Logger.log(`📖 Swagger documentation is available on: http://localhost:${port}/${globalPrefix}/docs`);
+  } catch (error) {
+    Logger.error(`❌ Failed to start the application: ${error}`, '', 'Bootstrap', false);
+  }
 }
 
 bootstrap();
