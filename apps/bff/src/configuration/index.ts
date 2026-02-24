519bd8123626eb9580e7a6925351d56b7877a052
Hai Thanh <haithanh.nguyen@estuary.solutions>
2026-02-24 17:29:22 +0700

    chore/#configuration: Setup env config

---
+++ apps/bff/src/configuration/index.ts
@@ -0,0 +1,17 @@
+class Configuration {
+  NODE_ENV: string;
+  IS_DEV: boolean;
+  GLOBAL_PREFIX: string;
+  PORT: number;
+
+  constructor() {
+    this.NODE_ENV = process.env.NODE_ENV || 'development';
+    this.IS_DEV = this.NODE_ENV === 'development';
+    this.GLOBAL_PREFIX = process.env.GLOBAL_PREFIX || 'api/v1';
+    this.PORT = process.env.PORT ? Number(process.env.PORT) : 3300;
+  }
+}
+
+export const CONFIGURATION = new Configuration();
+
+export type TConfiguration = typeof CONFIGURATION;
