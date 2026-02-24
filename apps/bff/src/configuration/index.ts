9bdc9654b5f330c763ff68aae17617228221aca3
Hai Thanh <haithanh.nguyen@estuary.solutions>
2026-02-24 17:49:03 +0700

    chore/#configuration: Restructure for extends base env configuration

--- apps/bff/src/configuration/index.ts
+++ apps/bff/src/configuration/index.ts
@@ -1,15 +1,7 @@
-class Configuration {
-  NODE_ENV: string;
-  IS_DEV: boolean;
-  GLOBAL_PREFIX: string;
-  PORT: number;
-
-  constructor() {
-    this.NODE_ENV = process.env.NODE_ENV || 'development';
-    this.IS_DEV = this.NODE_ENV === 'development';
-    this.GLOBAL_PREFIX = process.env.GLOBAL_PREFIX || 'api/v1';
-    this.PORT = process.env.PORT ? Number(process.env.PORT) : 3300;
-  }
+import { BaseConfiguration } from '@common/configuration/base.config';
+import { AppConfiguration } from '@common/configuration/app.config';
+class Configuration extends BaseConfiguration {
+  APP_CONFIG = new AppConfiguration();
 }
 
 export const CONFIGURATION = new Configuration();
